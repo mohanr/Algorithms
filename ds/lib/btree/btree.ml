05549c7a5d2213b8393e914aa88e2e947034e268
Aarushi <anu@Aarushis-MacBook-Pro.local>
2024-02-22 19:43:04 +0530

    Minimal BTree

--- ds/lib/btree/btree.ml
+++ ds/lib/btree/btree.ml
@@ -1,39 +1,45 @@
 open Stdlib
 
-type 'v values = Array
-type 'k keys = Array
+type 'v values = 'v array
+type 'k keys = 'k array
 
 
-type ('k, 'v) leaf = 
+type ('k, 'v) leaf= 
   | LeafKeys of ('k) keys 
   | LeafValues of ('v) values 
   | LeafNext of  ('k, 'v) t
 and ('k, 'v) t =
   | Leaf of ('k, 'v) leaf
-
+  | Empty
 
 
 type  ('k, 'v) node =
-    | NodeKeys   of ('k) keys 
-    | NodeChilds of ('k, 'v) children 
+      ('k) keys *  ('k, 'v) children
 and
   ('k, 'v) children =   ('k, 'v) bTree Array.t
 and ('k, 'v) bTree =
   | BTreeNode of ('k, 'v) node 
   | BTreeLeaf of ('k, 'v) leaf
 
-let keys ls  =
- Array.of_list
-
+let keys : 'b list -> 'b keys   =
+  Array.of_list
 
 let values ls  =
   Array.of_list
 
-let leaves leafls =
-  List.map (fun leaf -> BTreeLeaf leaf ) leafls
+let leaves leafls:('k, 'v) bTree array
+=
+  List.map (fun leaf : ('k, 'v) bTree  -> leaf  ) leafls
   |> Array.of_list
 
-type 'a lazy_dlist_value =
-  | Dnil
-  | Dnode of 'a lazy_dlist_value * 'a * 'a lazy_dlist_value
-and 'a lazy_dlist = 'a lazy_dlist_value Lazy.t
+let leafs k =
+ BTreeLeaf (LeafKeys (k))
+
+let btreenode keys children=
+  BTreeNode (  keys, children)
+
+
+let btree_example () =
+  let leaf1 = leafs (keys [1; 2]) in
+  let root = btreenode  (keys [3; 5])  (leaves [leaf1]) in
+  root
