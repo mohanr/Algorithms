2846c9abcdfe31b3d167baf30f7d78bfc5b1440e
mohanr <anu@Aarushis-MacBook-Pro.local>
2026-01-02 19:31:38 +0530

    Generate range

--- ds/test/interval_test.ml
+++ ds/test/interval_test.ml
@@ -14,8 +14,63 @@ let%expect_test "Test min/max range"=
   in range  (List.of_enum(1--2) : int list) (List.of_enum(3--4) : int list)
 
 let%expect_test "Test generate range"=
-  let data()  =
-    let d = int 32 |> fun a -> []@[a] |>  List.reduce ( max Int.ord ) |> fun m -> (0--m) in
-    List.iter (fun x -> Printf.printf "%d" x ) ((List.of_enum d) : int list) ;
-    [%expect {| 01234567891011 |}]
-  in data()
+    let () = self_init() in
+    let r = int 32 in
+    let rec data x =
+      if x <= 10 then(
+       let d = r |> fun a -> []@[a] |>  List.reduce ( max Int.ord ) |>
+                                  fun m -> ((int (m - 1))--m) in
+       List.iter (fun x -> Printf.printf "%d" x ) (List.of_enum d : int list) ;
+       [%expect {|
+         (* CR expect_test: Test ran multiple times with different test outputs *)
+         ============================= Output 1 / 11 =============================
+         9101112
+         ============================= Output 2 / 11 =============================
+         9101112
+         ============================= Output 3 / 11 =============================
+         3456789101112
+         ============================= Output 4 / 11 =============================
+         123456789101112
+         ============================= Output 5 / 11 =============================
+         0123456789101112
+         ============================= Output 6 / 11 =============================
+         789101112
+         ============================= Output 7 / 11 =============================
+         56789101112
+         ============================= Output 8 / 11 =============================
+         89101112
+         ============================= Output 9 / 11 =============================
+         101112
+         ============================ Output 10 / 11 =============================
+         123456789101112
+         ============================ Output 11 / 11 =============================
+         56789101112
+         |}];
+       data (x + 1)
+     )else ()
+    in data 0
+    (* (max in 1usize..32)(max in Just(max), *)
+    (*  ranges in vec(range(max), 10), point in 0..max) -> *)
+    (*   (usize, Vec<Range<usize>>, usize) { *)
+    (*         (max, ranges, point) *)
+
+(*     proptest! { *)
+(*         #[test] *)
+(*         fn works_once((max, ranges, point) in data()) { *)
+(*             let mut itree_output: Vec<usize> = *)
+(*                 IntervalTree::new(max, ranges.iter().cloned().enumerate()) *)
+(*                     .drain_containing(point) *)
+(*                     .collect(); *)
+(*             itree_output.sort(); *)
+
+(*             let expected_output: Vec<usize> = ranges *)
+(*                 .into_iter() *)
+(*                 .enumerate() *)
+(*                 .filter(|(_, range)| range.contains(&point)) *)
+(*                 .map(|(id, _)| id) *)
+(*                 .collect(); *)
+
+(*             prop_assert_eq!(itree_output, expected_output); *)
+(*         } *)
+(*     } *)
+(* } *)
