bec0adb79ebaff06ccf26dd8941376bc4f8aa16b
mohanr <anu@Aarushis-MacBook-Pro.local>
2026-01-02 16:45:38 +0530

    Initial interval tree test

--- ds/test/interval_test.ml
+++ ds/test/interval_test.ml
@@ -1,7 +1,8 @@
 open Batteries
 open BatOrd
+open BatRandom
 
-let%expect_test "Test range"=
+let%expect_test "Test min/max range"=
 
   let range a b =
       let combined_list = List.append a b in
@@ -9,5 +10,12 @@ let%expect_test "Test range"=
       let max_list = List.reduce( max Int.ord ) combined_list in
       List.iter (fun x -> Printf.printf "%d" x ) (List.of_enum(min_list--max_list) : int list);
       Printf.printf " %d %d" min_list max_list;
-      [%expect {| 12341 4 |}]
+      [%expect {| 1234 1 4 |}]
   in range  (List.of_enum(1--2) : int list) (List.of_enum(3--4) : int list)
+
+let%expect_test "Test generate range"=
+  let data()  =
+    let d = int 32 |> fun a -> []@[a] |>  List.reduce ( max Int.ord ) |> fun m -> (0--m) in
+    List.iter (fun x -> Printf.printf "%d" x ) ((List.of_enum d) : int list) ;
+    [%expect {| 01234567891011 |}]
+  in data()
