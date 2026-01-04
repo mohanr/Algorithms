7201f5272b2a359378fc0215b5cb69899d975523
mohanr <anu@Aarushis-MacBook-Pro.local>
2026-01-04 15:49:34 +0530

    Generate new range

--- ds/lib/interval/interval_tree.ml
+++ ds/lib/interval/interval_tree.ml
@@ -1,11 +1,13 @@
 open Containers
+open Base
+open Float
 open Batteries
 
 module  Node_vector = CCVector
 
 type node = {
-    by_start : int64 Node_vector.vector;
-    by_end : int64 Node_vector.vector
+    by_start : (int * int) Node_vector.vector;
+    by_end : (int * int) Node_vector.vector
 }
 [@@deriving show]
 module  Interval_vector = CCVector
@@ -18,7 +20,7 @@ type interval_tree = {
 
 module IntervalTree = struct
 
-    let new_range max ranges =
+    let new_range (m_ax : int) ranges =
       let  node =
         {
             by_start =  Node_vector.create();
@@ -29,18 +31,32 @@ module IntervalTree = struct
        nodes = Interval_vector.create(); (* TODO Pre-allocation ? *)
 
       } in
-      CCVector.push interval_tree.nodes node
-
-      let ranger ranges  =
-        let loop_while ra =
-
-        let tuple_range = BatTuple.Tuple2.of_enum ra in
-        BatEnum.iter (fun r ->
-          match r with
-            |(id, range) -> Printf.printf "ID %d" id
-        )
-        in
-        loop_while ranges
-
+      let () = CCVector.push interval_tree.nodes node in
+      let () =
+      List.iter (fun r ->
+          (match r with
+          | id,ranger -> let range : int list = List.of_enum ranger in
+                            if Int.(>) (List.nth  (List.rev range) 0)  m_ax  then(
+                               failwith "range out of bounds"
+                            )
+                            else(
+                               let mx : int = 1 in
+                               let last: int = List.last range in
+                               let first: int = List.first range in
+                               let mid : int = Int.sub (Int.of_float( log2 (Int.to_float (last  land ( mx lsl (first lor last))))))  mx in
+                               let n = CCVector.get interval_tree.nodes mid in
+                               let () = CCVector.push n.by_start (first, id) in
+                               CCVector.push n.by_end (last, id);
+                           )
+        )) ranges in
+      let () = CCVector.iter (fun vec ->
+                                CCVector.sort' ( fun (start, _) (start1, _) ->
+                                                 Int.compare start start1
+                                               ) vec.by_start) interval_tree.nodes in (*  Descending*)
+      let () = CCVector.iter (fun vec ->
+                                CCVector.sort' ( fun (_,en_d) ( _,en_d1) ->
+                                                 Int.compare en_d en_d1
+                                               ) vec.by_end) interval_tree.nodes in (*  Descending*)
+        ()
 
 end
--- ds/test/interval_test.ml
+++ ds/test/interval_test.ml
@@ -13,64 +13,65 @@ let%expect_test "Test min/max range"=
       [%expect {| 1234 1 4 |}]
   in range  (List.of_enum(1--2) : int list) (List.of_enum(3--4) : int list)
 
+let generate_point()  =
+    let () = self_init() in
+    int 32
+
+let generate_series r =
+         let d = r + 1 |> fun a -> []@[a] (*  Needs a list here *)
+                             |>  List.reduce ( max Int.ord ) |> (*  Find max*)
+                                  fun m -> ((int (m - 1))--m) in (*  Start < end, Start at 1 by adding 1 to random *)
+                                                                     (*  at the beginning *)
+         d
+
+let generate_range()  =
+    let () = self_init() in
+    let r = int 32 in
+    let series = [] in
+    let rec data x =            (*  Generate 10 ranges*)
+      if x <= 10 then(
+       let _ = series@[x,(generate_series r)] in
+       data (x + 1)
+     )else series
+    in data 0
+
 let%expect_test "Test generate range"=
     let () = self_init() in
     let r = int 32 in
-    let rec data x =
+    let series = CCVector.create() in
+    let rec data x =            (*  Generate 10 ranges*)
       if x <= 10 then(
-       let d = r |> fun a -> []@[a] |>  List.reduce ( max Int.ord ) |>
-                                  fun m -> ((int (m - 1))--m) in
-       List.iter (fun x -> Printf.printf "%d" x ) (List.of_enum d : int list) ;
+       let () = CCVector.push series (generate_series r) in
+       CCVector.iter (fun y  ->
+                          (List.iteri (fun x i -> Printf.printf "[%d] %d " i  x )  (List.of_enum y : int list))
+       ) series;
+
+       Printf.printf "Iteration is %d" x;
        [%expect {|
          (* CR expect_test: Test ran multiple times with different test outputs *)
          ============================= Output 1 / 11 =============================
-         9101112
+         [19] 0 [20] 1 [21] 2 [22] 3 [23] 4 [24] 5 [25] 6 [26] 7 [27] 8 [28] 9 [29] 10 [30] 11 [31] 12 Iteration is 0
          ============================= Output 2 / 11 =============================
-         9101112
+         [19] 0 [20] 1 [21] 2 [22] 3 [23] 4 [24] 5 [25] 6 [26] 7 [27] 8 [28] 9 [29] 10 [30] 11 [31] 12 Iteration is 1
          ============================= Output 3 / 11 =============================
-         3456789101112
+         [23] 0 [24] 1 [25] 2 [26] 3 [27] 4 [28] 5 [29] 6 [30] 7 [31] 8 Iteration is 2
          ============================= Output 4 / 11 =============================
-         123456789101112
+         [13] 0 [14] 1 [15] 2 [16] 3 [17] 4 [18] 5 [19] 6 [20] 7 [21] 8 [22] 9 [23] 10 [24] 11 [25] 12 [26] 13 [27] 14 [28] 15 [29] 16 [30] 17 [31] 18 Iteration is 3
          ============================= Output 5 / 11 =============================
-         0123456789101112
+         [8] 0 [9] 1 [10] 2 [11] 3 [12] 4 [13] 5 [14] 6 [15] 7 [16] 8 [17] 9 [18] 10 [19] 11 [20] 12 [21] 13 [22] 14 [23] 15 [24] 16 [25] 17 [26] 18 [27] 19 [28] 20 [29] 21 [30] 22 [31] 23 Iteration is 4
          ============================= Output 6 / 11 =============================
-         789101112
+         [25] 0 [26] 1 [27] 2 [28] 3 [29] 4 [30] 5 [31] 6 Iteration is 5
          ============================= Output 7 / 11 =============================
-         56789101112
+         [24] 0 [25] 1 [26] 2 [27] 3 [28] 4 [29] 5 [30] 6 [31] 7 Iteration is 6
          ============================= Output 8 / 11 =============================
-         89101112
+         [4] 0 [5] 1 [6] 2 [7] 3 [8] 4 [9] 5 [10] 6 [11] 7 [12] 8 [13] 9 [14] 10 [15] 11 [16] 12 [17] 13 [18] 14 [19] 15 [20] 16 [21] 17 [22] 18 [23] 19 [24] 20 [25] 21 [26] 22 [27] 23 [28] 24 [29] 25 [30] 26 [31] 27 Iteration is 7
          ============================= Output 9 / 11 =============================
-         101112
+         [15] 0 [16] 1 [17] 2 [18] 3 [19] 4 [20] 5 [21] 6 [22] 7 [23] 8 [24] 9 [25] 10 [26] 11 [27] 12 [28] 13 [29] 14 [30] 15 [31] 16 Iteration is 8
          ============================ Output 10 / 11 =============================
-         123456789101112
+         [28] 0 [29] 1 [30] 2 [31] 3 Iteration is 9
          ============================ Output 11 / 11 =============================
-         56789101112
+         [28] 0 [29] 1 [30] 2 [31] 3 Iteration is 10
          |}];
        data (x + 1)
      )else ()
     in data 0
-    (* (max in 1usize..32)(max in Just(max), *)
-    (*  ranges in vec(range(max), 10), point in 0..max) -> *)
-    (*   (usize, Vec<Range<usize>>, usize) { *)
-    (*         (max, ranges, point) *)
-
-(*     proptest! { *)
-(*         #[test] *)
-(*         fn works_once((max, ranges, point) in data()) { *)
-(*             let mut itree_output: Vec<usize> = *)
-(*                 IntervalTree::new(max, ranges.iter().cloned().enumerate()) *)
-(*                     .drain_containing(point) *)
-(*                     .collect(); *)
-(*             itree_output.sort(); *)
-
-(*             let expected_output: Vec<usize> = ranges *)
-(*                 .into_iter() *)
-(*                 .enumerate() *)
-(*                 .filter(|(_, range)| range.contains(&point)) *)
-(*                 .map(|(id, _)| id) *)
-(*                 .collect(); *)
-
-(*             prop_assert_eq!(itree_output, expected_output); *)
-(*         } *)
-(*     } *)
-(* } *)
